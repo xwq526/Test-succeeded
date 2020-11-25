a270b152c923eb00fc856f7454aa508d9d4212e4
xwq <1561493487@qq.com>
2020-11-25 23:40:18 +0800

    Tomcat配置,html,css,js

---
+++ maven-test/src/main/java/org/example/servlet/LoginServlet.java
@@ -0,0 +1,7 @@
+package org.example.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+}
